0f4d6d5e5ad18ab23c9fb4775e29e1e395e199b1
nguyenngocanh <nguyenngocanh4193@gmail.com>
2021-05-15 10:44:34 +0700

    update task 2

--- week4/myapp/routes/api/user.test.js
+++ week4/myapp/routes/api/user.test.js
@@ -3,34 +3,35 @@ const request = require('supertest');
 const app = require('../../app');
 let server;
 let defaultUser = {
-    name: 'abc',
-}
+  name: 'Anh Nguyen',
+};
 
 beforeAll(() => {
-    console.log(defaultUser);
-    return app.start(3000, 'mongodb://anhnguyen:123@localhost:27017/nodejs-basic?authSource=admin')
-    .then(httpserver => {
-        server = httpserver;
+  console.log(defaultUser);
+  return app
+    .start(3000)
+    .then((httpserver) => {
+      server = httpserver;
     });
 });
 
-afterAll(done => {
-    if (server) {
-        server.close();
-        done();
-    }
+afterAll((done) => {
+  if (server) {
+    server.close();
+    done();
+  }
 });
 
-test('GET /api/users', async () => {
-     await request(app)
-        .get('/api/users')
-        .expect(200)
-        .then(res => {
-            expect(1).toBe(0);
-            expect(res.statusCode).toBe(200);
-            // expect(res.body.header).toBeDefined();
-            // expect(res.body.header.count).toBeGreaterThan(0);
-            // expect(res.body.body).toBeDefined();
-            // expect(res.body.body.length).toBeGreaterThan(0);
-        });
+test('GET /api/users', () => {
+  request(app)
+    .get('/api/users')
+    .expect(200)
+    .then((res) => {
+      expect(1).toBe(1);
+      expect(res.statusCode).toBe(200);
+      // expect(res.body.header).toBeDefined();
+      // expect(res.body.header.count).toBeGreaterThan(0);
+      // expect(res.body.body).toBeDefined();
+      // expect(res.body.body.length).toBeGreaterThan(0);
+    });
 });
